357af067034e3ec344447299d48cc7c9f1271721
natvaran <nongdearnatvarun@gmail.com>
2018-01-12 16:06:35 +0700

    Finish 12Jan

--- app/src/main/java/natvaran/sbc/ac/th/sbcmoneychang/MainActivity.java
+++ app/src/main/java/natvaran/sbc/ac/th/sbcmoneychang/MainActivity.java
@@ -3,11 +3,25 @@ package natvaran.sbc.ac.th.sbcmoneychang;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import natvaran.sbc.ac.th.sbcmoneychang.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Add Fragment to Activity
+        if (savedInstanceState==null){
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment,new MainFragment())
+                    .commit();
+
+        }
+
+
+    }//Main Method
+
+}//Main Class
